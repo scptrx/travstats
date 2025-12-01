544b7ae0d9b80f9fd4939ef285ca0eb01cdd5e7c
scptrx <korbutcz@gmail.com>
2025-12-01 17:45:48 +0100

    map changes

--- frontend/script/map.js
+++ frontend/script/map.js
@@ -1,6 +1,10 @@
 const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://demotiles.maplibre.org/globe.json',
+    style: 'https://demotiles.maplibre.org/style.json', // плоский стиль
     center: [20, 49],
-    zoom: 6
+    zoom: 6,
+    pitch: 0,      // наклон камеры 0° 
+    bearing: 0     // поворот карты 0°
 });
+
+map.dragRotate.disable();  
