52bb1ca450320419e57c3d377fdd0b2502b04bc9
scptrx <korbutcz@gmail.com>
2025-12-02 16:52:25 +0100

    geojson added!! you can now select countries

--- frontend/script/map.js
+++ frontend/script/map.js
@@ -1,26 +1,129 @@
 const MAPTILER_API_KEY = 'LHHpvjCDKVfuiPq4D8wa';
 
+const blackMapStyle = 'https://api.maptiler.com/maps/019adaf0-84d9-7ddb-801a-02b5770c2fc3/style.json?key=' + MAPTILER_API_KEY;
+const whiteMapStyle = 'https://api.maptiler.com/maps/019adaf0-d3f6-7618-98a2-99ef9942c4d8/style.json?key=' + MAPTILER_API_KEY;
+const satelliteMapStyle = 'https://api.maptiler.com/maps/019adb04-1d43-78c4-9ab6-9e2070be2ee3/style.json?key=' + MAPTILER_API_KEY;
+
 const map = new maplibregl.Map({
     container: 'map',
-    style: 'https://api.maptiler.com/maps/019adaf0-84d9-7ddb-801a-02b5770c2fc3/style.json?key=' + MAPTILER_API_KEY, 
+    style: whiteMapStyle, 
     center: [20, 49],
     zoom: 6,
     pitch: 0,     
     bearing: 0     
 });
 
-document.getElementById('whiteButton').onclick = () => {
-    map.setStyle('https://api.maptiler.com/maps/019adaf0-d3f6-7618-98a2-99ef9942c4d8/style.json?key=' + MAPTILER_API_KEY    );
-};
-
-document.getElementById('blackButton').onclick = () => {
-    map.setStyle('https://api.maptiler.com/maps/019adaf0-84d9-7ddb-801a-02b5770c2fc3/style.json?key=' + MAPTILER_API_KEY    );
-};
-
-document.getElementById('satelliteButton').onclick = () => {
-    map.setStyle('https://api.maptiler.com/maps/019adb04-1d43-78c4-9ab6-9e2070be2ee3/style.json?key=' + MAPTILER_API_KEY    );
+function setMapStyle(styleURL) {
+    map.setStyle(styleURL);
+    map.once('styledata', addCountryLayers);
 }
-map.dragRotate.disable();  
+
+document.getElementById('whiteButton').onclick = () => setMapStyle(whiteMapStyle);
+document.getElementById('blackButton').onclick = () => setMapStyle(blackMapStyle);
+document.getElementById('satelliteButton').onclick = () => setMapStyle(satelliteMapStyle);
+
+
+map.dragRotate.disable(); 
+
+function addCountryLayers() {
+    if (!map.getSource('countries')) {
+        map.addSource('countries', {
+            type: 'geojson',
+            data: 'assets/geodata/countries_110.geojson'
+        });
+    }
+
+    // Слой всех границ
+    if (!map.getLayer('countries-border')) {
+        map.addLayer({
+            id: 'countries-border',
+            type: 'line',
+            source: 'countries',
+            paint: {
+                'line-color': '#ff8f1eff',
+                'line-width': 1
+            }
+        });
+    }
+
+    // Hover граница
+    if (!map.getLayer('hover-border')) {
+        map.addLayer({
+            id: 'hover-border',
+            type: 'line',
+            source: 'countries',
+            paint: {
+                'line-color': '#ff8f1eff',
+                'line-width': 3
+            },
+            filter: ['==', 'adm0_a3', '']
+        });
+    }
+
+    // Click fill (выделенная страна)
+    if (!map.getLayer('highlight-country')) {
+        map.addLayer({
+            id: 'highlight-country',
+            type: 'fill',
+            source: 'countries',
+            paint: {
+                'fill-color': '#ffc170ff',
+                'fill-opacity': 0.6
+            },
+            filter: ['==', 'adm0_a3', '']
+        });
+    }
+
+    // Hover fill (невидимая, для ловли мышки)
+    if (!map.getLayer('countries-fill')) {
+        map.addLayer({
+            id: 'countries-fill',
+            type: 'fill',
+            source: 'countries',
+            paint: {
+                'fill-color': '#ffc170ff',
+                'fill-opacity': 0.2
+            }
+        });
+    }
+
+    // Hover обработчик на fill
+    if (!map.hoverHandlerAdded) {
+        map.on('mousemove', 'countries-fill', (e) => {
+            if (!e.features || !e.features.length) {
+                map.setFilter('hover-border', ['==', 'adm0_a3', '']);
+                return;
+            }
+            const code = e.features[0].properties.adm0_a3;
+            map.setFilter('hover-border', ['==', 'adm0_a3', code]);
+        });
+
+        map.on('mouseleave', 'countries-fill', () => {
+            map.setFilter('hover-border', ['==', 'adm0_a3', '']);
+        });
+
+        map.hoverHandlerAdded = true;
+    }
+
+    // Click обработчик на fill
+    if (!map.clickHandlerAdded) {
+        map.on('click', 'countries-fill', (e) => {
+            if (!e.features || !e.features.length) return;
+            const props = e.features[0].properties;
+            const code = props.adm0_a3;
+            const name = props.name;
+
+            map.setFilter('highlight-country', ['==', 'adm0_a3', code]);
+            console.log("CLICKED:", name, code);
+        });
+
+        map.clickHandlerAdded = true;
+    }
+}
+
+// Инициализация
+map.on('load', addCountryLayers);
+
 
 // from oficial docs
 
